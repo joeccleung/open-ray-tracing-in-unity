ed57eb690c1017dc413c49d09a56258366a1a0ed
joeccleung <joeccleung@gmail.com>
2020-07-16 17:06:40 -0700

    47 - Spawn 1000 spheres

---
+++ Assets/Example/Playground/Scripts/Spawn1000Spheres.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn1000Spheres : MonoBehaviour
+{
+    [SerializeField] private int height = 0;
+    [SerializeField] private int width = 0;
+    [SerializeField] private GameObject prefab;
+
+    void Start()
+    {
+        float spacing = 5;
+
+        for(int w = 0; w < width; w++)
+        {
+            for(int h = 0; h < height; h++)
+            {
+                Instantiate(prefab, new Vector3(w * spacing, 0, h * spacing), Quaternion.identity);
+            }
+        }
+    }
+}
