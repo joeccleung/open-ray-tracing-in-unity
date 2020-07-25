16d55271ec23839c7533d4d92da332c1a2eedbdc
joeccleung <joeccleung@gmail.com>
2020-07-24 23:08:02 -0700

    Update test scene to test 1000 spheres scene

--- Assets/Example/Playground/Scripts/Spawn1000Spheres.cs
+++ Assets/Example/Playground/Scripts/Spawn1000Spheres.cs
@@ -2,21 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn1000Spheres : MonoBehaviour
-{
+public class Spawn1000Spheres : MonoBehaviour {
     [SerializeField] private int height = 0;
     [SerializeField] private int width = 0;
+    [SerializeField] private bool scattered = false;
     [SerializeField] private GameObject prefab;
 
-    void Start()
-    {
+    void Start() {
         float spacing = 5;
 
-        for(int w = 0; w < width; w++)
-        {
-            for(int h = 0; h < height; h++)
-            {
-                Instantiate(prefab, new Vector3(w * spacing, 0, h * spacing), Quaternion.identity);
+        for (int w = 0; w < width; w++) {
+            for (int h = 0; h < height; h++) {
+                Vector3 position = Vector3.zero;
+                if (scattered) {
+                    position = new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), Random.Range(-100, 100));
+                } else {
+                    position = new Vector3(w * spacing, 0, h * spacing);
+                }
+                Instantiate(prefab, position, Quaternion.identity);
             }
         }
     }
