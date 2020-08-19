0141ae15eb3bc2d64f0d0d0a64573b89b3b16f4b
joeccleung <joeccleung@gmail.com>
2020-08-19 07:50:04 -0700

    66 - Translucent Closest Hit Shader

---
+++ Assets/Example/Playground/Materials/TranslucentMat.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenRT;
+
+public class TranslucentMat : RTMaterial
+{
+   [SerializeField] public float reflectivity = 0;
+   [SerializeField] public float secondaryRayEffect = 0;
+   [SerializeField] public float transparency = 0;
+}
