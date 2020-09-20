41657eae3202f6dccfef24c41d33a7bcd6fe1bbc
Joe Leung <joe.leung@smashingideas.com>
2020-09-20 11:26:45 -0700

    74 - Volumentric rendering experiment

---
+++ Assets/SRP/AssetsDatabase/Editor/RTFrameworkMenuItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace OpenRT
+{
+    public class RTFrameworkMenuItem : MonoBehaviour
+    {
+        private static OpenRT.ClosetHitShaderCollectionGPUProgramGenerator closetHitShaderCollectionGPUProgramGenerator = new OpenRT.ClosetHitShaderCollectionGPUProgramGenerator();
+
+        [MenuItem("OpenRT/Refresh Shader Collection")]
+        static void RefreshShaderCollection()
+        {
+            closetHitShaderCollectionGPUProgramGenerator.ExportShaderCollection(CustomShaderDatabase.Instance.ShaderSortedByName(EShaderType.ClosestHit),
+                                                                                CustomShaderDatabase.Instance.ShaderMetaList(EShaderType.ClosestHit));
+        }
+    }
+
+}
---
+++ Assets/SRP/Data/RTRay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OpenRT
+{
+    public struct RTRay
+    {
+        Vector3 color;
+        Vector3 origin;
+        Vector3 direction;
+        float tmin;
+        float tmax;
+        uint type;
+        float weight;
+        uint gen;
+    
+        public static int Stride
+        {
+            get {
+                return sizeof(float) * (3 + 3 + 3 + 1 + 1 + 1) + sizeof(uint) * (1 + 1);
+            }
+        }
+    }
+}
+
