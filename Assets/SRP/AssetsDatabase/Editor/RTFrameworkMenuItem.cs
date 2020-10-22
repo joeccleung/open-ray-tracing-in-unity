f11da166d7a091e127747cf08fa997b13782bc65
Joe Leung <joe.leung@smashingideas.com>
2020-10-22 08:51:14 -0700

    Menu - Refresh Shader Collection button will reload the database and intersect shader collection

--- Assets/SRP/AssetsDatabase/Editor/RTFrameworkMenuItem.cs
+++ Assets/SRP/AssetsDatabase/Editor/RTFrameworkMenuItem.cs
@@ -7,13 +7,22 @@ namespace OpenRT
 {
     public class RTFrameworkMenuItem : MonoBehaviour
     {
-        private static OpenRT.ClosetHitShaderCollectionGPUProgramGenerator closetHitShaderCollectionGPUProgramGenerator = new OpenRT.ClosetHitShaderCollectionGPUProgramGenerator();
+        private static ClosetHitShaderCollectionGPUProgramGenerator closetHitShaderCollectionGPUProgramGenerator = new ClosetHitShaderCollectionGPUProgramGenerator();
+        private static IntersectShaderCollectionGPUProgramGenerator intersectShaderCollectionGPUProgramGenerator = new IntersectShaderCollectionGPUProgramGenerator();
 
         [MenuItem("OpenRT/Refresh Shader Collection")]
         static void RefreshShaderCollection()
         {
+            // (1) - Reload the shader database
+            // (2) - Rebuild the Closest Hit Shader Collection GPU Program
+            // (3) - Rebuild the Intersect Hit Shader Collection GPU Program
+
+            CustomShaderDatabase.Instance.LoadShaderDatabase();
             closetHitShaderCollectionGPUProgramGenerator.ExportShaderCollection(CustomShaderDatabase.Instance.ShaderSortedByName(EShaderType.ClosestHit),
                                                                                 CustomShaderDatabase.Instance.ShaderMetaList(EShaderType.ClosestHit));
+
+            intersectShaderCollectionGPUProgramGenerator.ExportShaderCollection(CustomShaderDatabase.Instance.ShaderSortedByName(EShaderType.Intersect),
+                                                                                CustomShaderDatabase.Instance.ShaderMetaList(EShaderType.Intersect));
         }
     }
 
