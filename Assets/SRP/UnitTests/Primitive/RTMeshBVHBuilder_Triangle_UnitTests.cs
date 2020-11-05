af92c81169d473cff36d7feeafe532839327b60e
Joe Leung <joe.leung@smashingideas.com>
2020-11-05 07:16:14 -0800

    Setup Unit Tests for RTMeshBVHBuilder

---
+++ Assets/SRP/UnitTests/Primitive/RTMeshBVHBuilder_Triangle_UnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace OpenRT.UnitTests.Primitive
+{
+    public class RTMeshBVHBuilder_Triangle_UnitTests
+    {
+        [Test]
+        public void One_Triangle()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
