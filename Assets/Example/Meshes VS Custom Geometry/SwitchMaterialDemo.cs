a9a0de10b86fc4e17bb0e2237a11bf0fab2b507b
Joe Leung <joe.leung@smashingideas.com>
2021-04-18 22:41:38 -0700

    Example Scenes

---
+++ Assets/Example/Meshes VS Custom Geometry/SwitchMaterialDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using OpenRT;
+using UnityEngine;
+
+public class SwitchMaterialDemo : MonoBehaviour
+{
+    [SerializeField] private RTRenderer m_renderer;
+    [SerializeField] private RTMaterial m_phongMat;
+    [SerializeField] private RTMaterial m_translucentMat;
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_renderer.material = m_phongMat;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_renderer.material = m_translucentMat;
+        }
+
+        transform.localRotation *= Quaternion.Euler(1, 0, 0);
+    }
+}
